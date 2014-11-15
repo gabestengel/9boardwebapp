b767ce47fd3d5806ae726f0fe5f20b50e5031e30
Gabe Stengel <gabesten@gmail.com>
2014-11-15 12:37:55 -0500

    Save

--- Script_WelcomePage.js
+++ Script_WelcomePage.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-        $("#board1 td").click(function() {     
+        $("#board1 th").click(function() {     
  
             var column_num = parseInt( $(this).index() ) + 1;
             var row_num = parseInt( $(this).parent().index() )+1;    
  
-            $("#one").html( "Row_num =" + row_num + "  ,  Rolumn_num ="+ column_num );   
+            $("#one").text("X");   
         });
     });
